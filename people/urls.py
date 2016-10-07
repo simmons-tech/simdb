31359a553e9cf584f3b7d8425e9cdb635aa39309
Amol Bhave <ammubhave@gmail.com>
2016-10-07 13:03:35 -0400

    addd directory

--- people/urls.py
+++ people/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from people import views
 
 urlpatterns = [
-    #url(r'^$', views.home),
+    url(r'^$', views.directory, name='directory'),
+    url(r'^entry/(?P<username>[a-z]*)$', views.entry, name='directory_entry'),
 ]
--- people/views.py
+++ people/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def directory(request):
+    results = []
+    if request.method == 'POST':
+        results = ['amol']
+    return render(request, 'people/directory.html', {'results': results})
+
+
+def entry(request, username):
+    return render(request, 'people/entry.html')
